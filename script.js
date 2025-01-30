701957a2380a8eb3aaba66e0e54fdce4343b1979
ssj400 <gafetel2017@gmail.com>
2025-01-30 16:45:54 -0300

    Added functionality to the buttons
    Button reset, erase, normal and random working

--- script.js
+++ script.js
@@ -1,24 +1,72 @@
 let container = document.querySelector("#container");
-let boxAmount = 9;
 let changeGrids = document.querySelector(".button");
+let normal = document.querySelector(".normal")
+let random = document.querySelector(".random")
+let erase = document.querySelector(".erase")
+let reset = document.querySelector("#reset")
+let boxAmount = 9;
 let size = `55.5`;
+let box;
 
-const paintRed = (element) => {
+
+let buttonActivated;
+normal.classList.add("selected");
+
+random.addEventListener("click", () => {
+    buttonActivated = 1;
+    random.classList.add("selected");
+    normal.classList.remove("selected");
+    erase.classList.remove("selected");
+});
+
+normal.addEventListener("click", () => {
+    buttonActivated = 0;
+    normal.classList.add("selected")
+    random.classList.remove("selected");
+    erase.classList.remove("selected");
+});
+
+erase.addEventListener("click", () => {
+    buttonActivated = -1;
+    erase.classList.add("selected");
+    normal.classList.remove("selected");
+    random.classList.remove("selected");
+});
+
+
+const paint = (element) => {
     element.addEventListener("mouseover", () => {
-            element.classList.add("hovered");
-            element.style.background = `rgb(${Math.floor (Math.random() * 255)}, ${Math.floor (Math.random() * 255)}, ${Math.floor (Math.random() * 255)})`
+        let paintColor;
+        if (buttonActivated === 1) {
+            paintColor = `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`;
+        } else if (buttonActivated === 0){
+            paintColor = "white";
+        } else if (buttonActivated === -1) {
+            paintColor = "black";
+        }
+
+        element.style.background = paintColor;
     });
 }
 
+let resetAll = (element) => {
+    reset.addEventListener("click", () => {
+        element.style.background = 'black';
+    })
+}
+
+
+
+
 const createGrids = () => {
     for (let i = 0; i < (boxAmount ** 2); i++) {
-        let box = document.createElement("div");
+        box = document.createElement("div");
         box.classList.add("box");
         container.appendChild(box);
         box.style.width = `${size}px`;
 
-
-        paintRed(box);
+        paint(box);
+        resetAll(box);
 
     }
 }
@@ -35,3 +83,4 @@ changeGrids.addEventListener("click", () => {
 
     createGrids();
 })
+
